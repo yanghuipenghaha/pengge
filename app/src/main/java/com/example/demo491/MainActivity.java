55eaa11a3b6c334f79a267ad3844dc0e33f48628
basba <1804564524@qq.com>
2020-11-25 10:24:58 +0800

    哈哈哈哈哈.

---
+++ app/src/main/java/com/example/demo491/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.demo491;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
